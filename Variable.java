1fcca8c31edba1fb7e4d34ce25808df96eb03b2b
Adarsh <28adarshdhangar@gmail.com>
2023-06-02 00:44:06 +0530

    Variables in JAVA

---
+++ Variable.java
@@ -0,0 +1,19 @@
+//Declaration and Initialization of varibles......
+import java.lang.*;
+class Variable{
+
+	public static void main(String args[])
+	{
+		int i=18;
+		byte b=127;
+		float f=12.5;
+		long l=120021;
+		
+		System.out.println("Value of variable i : "+i);
+		System.out.println("Value of variable b : "+b);
+		System.out.println("Value of variable f : "+f);
+		System.out.println("Value of variable l : "+l);
+	}
+
+
+}
