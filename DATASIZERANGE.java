84ae7d6f0caa44feed75c8a1d4733ae56e340fad
Adarsh <28adarshdhangar@gmail.com>
2023-06-02 00:47:22 +0530

    DATA TYPES

---
+++ DATASIZERANGE.java
@@ -0,0 +1,23 @@
+//THIS PROGRAM WILL CONTAIN INFORMATION ABOUT DIFFERENT DATA TYPES PROVIDED BY JAVA
+import java.lang.*;
+class DATASIZERANGE{
+	public static void main(String args[])
+	{
+		System.out.println("Minimum INT : "+Integer.MIN_VALUE);	
+		System.out.println("Maximum INT : "+Integer.MAX_VALUE);
+		System.out.println("Size of INT Data_type : "+Integer.BYTES);
+		System.out.println();
+	
+		System.out.println("Minimum FLOAT : "+Float.MIN_VALUE);	
+		System.out.println("Maximum FLOAT : "+Float.MAX_VALUE);
+		System.out.println("Size of FLOAT Data_type : "+Float.BYTES);
+		System.out.println();
+
+		System.out.println("Minimum BYTE : "+Byte.MIN_VALUE);	
+		System.out.println("Maximum BYTE : "+Byte.MAX_VALUE);
+		System.out.println("Sizi of BYTE Data_type : "+Byte.BYTES);
+
+	}
+
+
+}
