1d7e1ca6be5f1babc2f34c95f9da542b0e6e9c2b
Adarsh <28adarshdhangar@gmail.com>
2023-06-02 00:48:31 +0530

    Input in JAVA

---
+++ Read.java
@@ -0,0 +1,16 @@
+//THIS PROGRAM EXPLAINS HOW TO TAKE INPUT FROM USER
+import java.lang.*;
+import java.util.*;
+class Read{
+	public static void main(String args[])
+	{
+	
+	String name;
+	System.out.print("Your name please : ");
+	Scanner a = new Scanner(System.in);
+	name = a.nextLine();
+	System.out.println("Welcome to coder's community "+name);
+	}
+
+
+}
