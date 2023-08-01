e9951d083cc5ea429bfcfdcff7f31f573cbf4465
Adarsh <28adarshdhangar@gmail.com>
2023-08-02 02:03:08 +0530

    Added way to access non static methods from main method

--- Methods/definition.java
+++ Methods/definition.java
@@ -6,12 +6,22 @@ class definition{
         else
             return b;
     }
+    void sum(int  a,int b){//non static method
+        System.out.print("Addition of numbers : "+(a+b));
+    }
     public static void main(String args[]){
         Scanner a=new Scanner(System.in);
         int num1,num2;
         System.out.print("Enter two numbers : ");
         num1=a.nextInt();
         num2=a.nextInt();
-        System.out.println("Greater number is : "+max(num1,num2));
+        System.out.println("Greater number is : "+max(num1,num2));//direct calling without obj. creation
+
+
+        definition obj=new definition();
+        obj.sum(num1,num2);//calling non static method from main(Static) method
     }
+
+
+    
 }
